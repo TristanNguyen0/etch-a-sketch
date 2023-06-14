ca99c500a44d9e13a9f12b5862b36fba95d87c94
TristanNguyen0 <xkaura64@gmail.com>
2023-06-13 20:24:27 -0400

    Use javascript to make a createGrid() function

---
+++ script.js
@@ -0,0 +1,21 @@
+//define important global variables
+const containerWidth = containerHeight = 860;
+const container = document.querySelector('.container');
+
+function createGrid(width, height){
+    for(let h=0;h<height;h++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        row.style.width = containerWidth;
+        container.appendChild(row);
+        for(let w=0;w<width;w++){
+            let elem = document.createElement('div');
+            elem.classList.add('row-element');
+            elem.style.width = containerWidth/width;
+            elem.style.height = containerHeight/height;
+            row.appendChild(elem);
+        }
+    }
+}
+
+createGrid(16,16);
